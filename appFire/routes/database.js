5ad4fc7169bf549a19284be6dccc55cf925da84e
mmol <mariusz@acmstudio.pl>
2020-04-26 09:52:57 +0200

    Database Connection

--- appFire/routes/database.js
+++ appFire/routes/database.js
@@ -1,5 +1,24 @@
 var express = require('express');
+var firebase =require('firebase-admin');
 var router = express.Router();
+
+var ServiceAccount = require('../database.json')
+
+
+//Initialization
+firebase.initializeApp({
+    credential: firebase.credential.cert(ServiceAccount),
+    databaseURL: "https://databaseproj-312c8.firebaseio.com/"
+});
+
+//seting up connection
+var db = firebase.database();
+var ref = db.ref('students');
+
+ref.once("value", function (snapshot) {
+    console.log(snapshot.val());
+});
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
