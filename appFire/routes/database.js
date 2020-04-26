6e4ed5b545cff34974c6a187ec5a63170913b5b4
mmol <mariusz@acmstudio.pl>
2020-04-26 10:56:38 +0200

    Pug rendering

--- appFire/routes/database.js
+++ appFire/routes/database.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var firebase =require('firebase-admin');
+var firebase = require('firebase-admin');
 var router = express.Router();
 
 var ServiceAccount = require('../database.json')
@@ -15,14 +15,16 @@ firebase.initializeApp({
 var db = firebase.database();
 var ref = db.ref('students');
 
-ref.once("value", function (snapshot) {
-    console.log(snapshot.val());
-});
+
 
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('Database route is working');
+router.get('/', function (req, res, next) {
+    ref.once("value", function (snapshot) {
+        console.log(snapshot.val());
+        res.render('database', { title: 'Firebase Example' });
+    })
+
 });
 
 module.exports = router;
